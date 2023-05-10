4f09a833b895698258380920dca2d42f73f61251
vinay-satva-4221 <vinayp08042@gmail.com>
2023-05-10 16:37:45 +0530

    done

--- image/TaskImage/index.js
+++ image/TaskImage/index.js
@@ -1,1 +1,5 @@
+import path from 'path';
+import url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export const taskImagePath = __dirname
--- image/Timelines/index.js
+++ image/Timelines/index.js
@@ -1,1 +1,6 @@
+import path from 'path';
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export const timelineImagePath = __dirname
