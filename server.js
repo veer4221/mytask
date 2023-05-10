f56b7b370a8cef32b4487ed2ea9dc32b95f0fe48
vinay-satva-4221 <vinayp08042@gmail.com>
2023-05-10 15:39:59 +0530

    done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { APP_PORT, MONGO_ATLAS_URL } from "./config/index.js";
 const app = express();
 import routes from "./routes/index.js";
-import errorHandler from "./middlewares/errorHandler";
+import errorHandler from "./middlewares/errorHandler.js";
 import mongoose from "mongoose";
 import cors from "cors";
 app.use(cors());
